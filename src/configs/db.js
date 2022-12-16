226911daaf0c1704f62fe1205852d6d5bcb687ff
Surya Srivastava <suryasrivastava724@gmail.com>
2022-12-16 22:42:34 +0530

    express app

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongose");
-
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
 const connect = () => {
   return mongoose.connect(
     "mongodb+srv://surya:surya123@cluster0.egl1mnc.mongodb.net/MOCK10"
