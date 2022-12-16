310d120713138d0d3581443b167d6d6fccfcd102
Surya Srivastava <suryasrivastava724@gmail.com>
2022-12-17 00:08:16 +0530

    signup,signin api

--- src/features/users/users.model.js
+++ src/features/users/users.model.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String },
-  email: { type: String },
-  pasword: { type: String },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  pasword: { type: String, required: true },
 });
 
 const User = mongoose.model("user", userSchema);
