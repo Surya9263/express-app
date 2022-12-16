eda1c116aec80e9d879b60a9a953be145db51dc5
Surya Srivastava <suryasrivastava724@gmail.com>
2022-12-16 22:58:01 +0530

    routing

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const connect = require("./src/configs/db");
+const userRoute = require("./src/features/users/users.route");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/users", userRoute);
 
-app.get("/", (req, res) => res.send("hello users"));
+// app.get("/", (req, res) => res.send("hello users"));
 
 const PORT = process.env.PORT || 8080;
 
