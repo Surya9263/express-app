81fe9a080b9260618baf456f99828b59cd55c393
Surya Srivastava <suryasrivastava724@gmail.com>
2022-12-16 22:37:10 +0530

    express app

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("hello"));
+app.get("/", (req, res) => res.send("hello users"));
+
+
 
 const PORT = process.env.PORT || 8080;
 
