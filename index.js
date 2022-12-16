fc393173df27977e85e519c22c593345b35b69b2
Surya Srivastava <suryasrivastava724@gmail.com>
2022-12-16 22:34:12 +0530

    express app

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => res.send('hello'))
+app.get("/", (req, res) => res.send("hello"));
 
-app.listen(8080, () => {console.log('server started on port 8080')})
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log("server started on port 8080");
+});
