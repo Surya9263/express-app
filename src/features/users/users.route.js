84e2f163e54d37b73a2dfdc85389413e61c50fdf
Surya Srivastava <suryasrivastava724@gmail.com>
2023-01-08 23:26:23 +0530

    updated

--- src/features/users/users.route.js
+++ src/features/users/users.route.js
@@ -7,14 +7,14 @@ const app = express.Router();
 app.post("/signup", async (req, res) => {
   let { name, email, password } = req.body;
   if (!name || !email || !password) {
-    return res.status(204).send({msg:"Each field must be filled"});
+    return res.status(204).send({ msg: "Each field must be filled" });
   }
   try {
     let user = await User.findOne({ email });
     if (user) {
       return res.status(404).send("Email already exists");
     }
-    let newUser = await User.create(req.body);
+    let newUser = await User.create({ name, email, password });
     res.send({ msg: "User created successfully...", user: newUser });
   } catch (error) {
     res.status(500).send(error.message);
